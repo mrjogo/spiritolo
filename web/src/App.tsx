4c0d15a6a6b0c491761dee88a2494648dd875aa1
Ruddick Lawrence <679360+mrjogo@users.noreply.github.com>
2026-04-22 19:09:31 +0000

    Web: App routes + catch-all 404

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import { RecipeList } from './pages/RecipeList';
+import { RecipeDetail } from './pages/RecipeDetail';
+import { ErrorPage } from './components/ErrorPage';
+
 export default function App() {
-  return <div>placeholder</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<RecipeList />} />
+      <Route path="/recipes/:id" element={<RecipeDetail />} />
+      <Route
+        path="*"
+        element={<ErrorPage title="Page not found" message="That URL doesn't match any page." />}
+      />
+    </Routes>
+  );
 }
